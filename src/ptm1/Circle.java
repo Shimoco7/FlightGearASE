587ef63e3882994716c8536b270c0899e2d10797
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-15 16:30:35 +0300

    Update Circle.java
    Adar: adding isCircleInside function

--- src/ptm1/Circle.java
+++ src/ptm1/Circle.java
@@ -4,9 +4,27 @@ public class Circle {
     public final float radius;
     public Point p;
 
-    public Circle( Point p, float radius) {
+	public Circle( Point p, float radius) {
         this.p = p;
         this.radius = radius;
     }
+	
+	   public Point getPoint() {
+			return p;
+		}
+
+		public float getRadius() {
+			return radius;
+		}
+		
+		public boolean isCircleInside(Circle second) {
+			   double distSq = Math.sqrt(Math.pow(this.getPoint().x - second.getPoint().x,2) * Math.pow(this.getPoint().y - second.getPoint().y,2));
+			   if (distSq + second.getRadius() == this.getRadius())
+			      return true;
+			      else if (distSq + second.getRadius() < this.getRadius())
+			         return true;
+			   else
+			      return false;
+			}
 
 }
