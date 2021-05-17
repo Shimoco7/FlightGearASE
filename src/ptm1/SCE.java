4cfa5a40269f0cb22e2c188ccc8871ac4f42d184
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-17 12:29:31 +0300

    Update SCE.java

--- src/ptm1/SCE.java
+++ src/ptm1/SCE.java
@@ -8,18 +8,12 @@ import java.util.Random;
 
 public class SCE {
 
-	/*
-	 * Returns the smallest circle that encloses all the given points. Runs in
-	 * expected O(n) time, randomized. Note: If 0 points are given, null is
-	 * returned. If 1 point is given, a circle of radius 0 is returned.
-	 */
-	// Initially: No boundary points known
 	public static Circle makeCircle(List<Point> points) {
-		// Clone list to preserve the caller's data, randomize order
+		
 		List<Point> shuffled = new ArrayList<>(points);
 		Collections.shuffle(shuffled, new Random());
 
-		// Progressively add points to circle or recompute circle
+		
 		Circle c = null;
 		for (int i = 0; i < shuffled.size(); i++) {
 			Point p = shuffled.get(i);
@@ -29,7 +23,7 @@ public class SCE {
 		return c;
 	}
 
-	// One boundary point known
+	
 	private static Circle makeCircleOnePoint(List<Point> points, Point p) {
 		Circle c = new Circle(p, 0);
 		for (int i = 0; i < points.size(); i++) {
@@ -44,19 +38,19 @@ public class SCE {
 		return c;
 	}
 
-	// Two boundary points known
+	
 	private static Circle makeCircleTwoPoints(List<Point> points, Point p, Point q) {
 		Circle circ = makeDiameter(p, q);
 		Circle left = null;
 		Circle right = null;
 
-		// For each point not in the two-point circle
+		
 		Point pq = q.subtract(p);
 		for (Point r : points) {
 			if (circ.contains(r))
 				continue;
 
-			// Form a circumcircle and classify it on left or right side
+			
 			float cross = (float) pq.cross(r.subtract(p));
 			Circle c = makeCircumcircle(p, q, r);
 			if (c == null)
@@ -67,7 +61,7 @@ public class SCE {
 				right = c;
 		}
 
-		// Select which circle to return
+		
 		if (left == null && right == null)
 			return circ;
 		else if (left == null)
@@ -84,7 +78,7 @@ public class SCE {
 	}
 
 	static Circle makeCircumcircle(Point a, Point b, Point c) {
-		// Mathematical algorithm from Wikipedia: Circumscribed circle
+		
 		float ox = (Math.min(Math.min(a.x, b.x), c.x) + Math.max(Math.max(a.x, b.x), c.x)) / 2;
 		float oy = (Math.min(Math.min(a.y, b.y), c.y) + Math.max(Math.max(a.y, b.y), c.y)) / 2;
 		float ax = a.x - ox, ay = a.y - oy;
