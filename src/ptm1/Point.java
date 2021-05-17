bf1d179445ee0d5e07cbe35690f1fbad46cb77a5
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-17 11:20:44 +0300

    Update Point.java

--- src/ptm1/Point.java
+++ src/ptm1/Point.java
@@ -1,9 +1,16 @@
 package ptm1;
 
 public class Point {
-    public final float x,y;
-    public Point(float x, float y) {
-        this.x=x;
-        this.y=y;
-    }
+	public final float x, y;
+
+	public Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
 }
