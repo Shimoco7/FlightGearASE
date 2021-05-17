01aeef08473ad92ec5a63c8e7dcc986f9baa36f8
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-17 12:17:57 +0300

    Update Point.java

--- src/ptm1/Point.java
+++ src/ptm1/Point.java
@@ -7,6 +7,20 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
+	public Point subtract(Point p) {
+		return new Point(x - p.x, y - p.y);
+	}
+
+
+	public double distance(Point p) {
+		return Math.hypot(x - p.x, y - p.y);
+	}
+
+
+	// Signed area / determinant thing
+	public double cross(Point p) {
+		return x * p.y - y * p.x;
+	}
 
 	@Override
 	public String toString() {
