cd009549660bff55acb7a644a2760ba93e596d18
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-17 11:21:36 +0300

    Update Circle.java
    Circle functions update

--- src/ptm1/Circle.java
+++ src/ptm1/Circle.java
@@ -1,30 +1,37 @@
 package ptm1;
 
 public class Circle {
-    public final float radius;
-    public Point p;
 
-	public Circle( Point p, float radius) {
-        this.p = p;
-        this.radius = radius;
-    }
-	
-	   public Point getPoint() {
-			return p;
-		}
+	public final float radius;
+	public Point p;
 
-		public float getRadius() {
-			return radius;
-		}
-		
-		public boolean isCircleInside(Circle second) {
-			   double distSq = Math.sqrt(Math.pow(this.getPoint().x - second.getPoint().x,2) * Math.pow(this.getPoint().y - second.getPoint().y,2));
-			   if (distSq + second.getRadius() == this.getRadius())
-			      return true;
-			      else if (distSq + second.getRadius() < this.getRadius())
-			         return true;
-			   else
-			      return false;
-			}
+	public Circle(Point p, float radius) {
+		this.p = p;
+		this.radius = radius;
+	}
+
+	public boolean isCircleInside(Circle second) {
+		double distSq = Math.sqrt(Math.pow(this.getPoint().x - second.getPoint().x, 2)
+				* Math.pow(this.getPoint().y - second.getPoint().y, 2));
+		if (distSq + second.getRadius() == this.getRadius())
+			return true;
+		else if (distSq + second.getRadius() < this.getRadius())
+			return true;
+		else
+			return false;
+	}
+
+	public Point getPoint() {
+		return p;
+	}
+
+	public float getRadius() {
+		return radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", p=" + p + "]";
+	}
 
 }
