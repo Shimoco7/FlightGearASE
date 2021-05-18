7bb0e0094a27b02c310bd8a8e4272b3b245e0466
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-18 19:15:58 +0300

    Update Circle.java

--- src/ptm1/Circle.java
+++ src/ptm1/Circle.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 public class Circle {
 
-	public final float radius;
+	public final float radius; //Circel's radius and center point
 	public Point p;
 	private static final double MULTIPLICATIVE_EPSILON = 1 + 1e-14;
 	public Circle(Point p, float radius) {
@@ -12,7 +12,7 @@ public class Circle {
 		this.radius = radius;
 	}
 
-	public boolean isCircleInside(Circle second) {
+	public boolean isCircleInside(Circle second) { //Checks if Circle second is contained in this circle
 		double distSq = Math.sqrt(Math.pow(this.getPoint().x - second.getPoint().x, 2)
 				* Math.pow(this.getPoint().y - second.getPoint().y, 2));
 		if (distSq + second.getRadius() == this.getRadius())
@@ -23,26 +23,26 @@ public class Circle {
 			return false;
 	}
 
-	public Point getPoint() {
+	public Point getPoint() { //Returns Circel's center point
 		return p;
 	}
 
-	public float getRadius() {
+	public float getRadius() { //Returns Circel's radius
 		return radius;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() { ////toString function
 		return "Circle [radius=" + radius + ", p=" + p + "]";
 	}
 	
 	
-    public boolean contains(Point p) {
+    public boolean contains(Point p) { //Checks if point is in the circle
         return p.distance(p) <= radius * MULTIPLICATIVE_EPSILON;
     }
 
 
-    public boolean contains(Collection<Point> ps) {
+    public boolean contains(Collection<Point> ps) { //Checks if few points is in the circle
         for (Point p : ps) {
             if (!contains(p))
                 return false;
