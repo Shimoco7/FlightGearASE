f9cd6c79abc392784e7a6354e737b8d725b07dd8
adargvili <63505828+adargvili@users.noreply.github.com>
2021-05-18 21:05:44 +0300

    Update SCE.java

--- src/ptm1/SCE.java
+++ src/ptm1/SCE.java
@@ -9,11 +9,10 @@ import java.util.Random;
 public class SCE {
 
 	public static Circle makeCircle(List<Point> points) {
-		
+
 		List<Point> shuffled = new ArrayList<>(points);
 		Collections.shuffle(shuffled, new Random());
 
-		
 		Circle c = null;
 		for (int i = 0; i < shuffled.size(); i++) {
 			Point p = shuffled.get(i);
@@ -23,7 +22,6 @@ public class SCE {
 		return c;
 	}
 
-	
 	private static Circle makeCircleOnePoint(List<Point> points, Point p) {
 		Circle c = new Circle(p, 0);
 		for (int i = 0; i < points.size(); i++) {
@@ -38,30 +36,28 @@ public class SCE {
 		return c;
 	}
 
-	
 	private static Circle makeCircleTwoPoints(List<Point> points, Point p, Point q) {
 		Circle circ = makeDiameter(p, q);
 		Circle left = null;
 		Circle right = null;
 
-		
 		Point pq = q.subtract(p);
 		for (Point r : points) {
 			if (circ.contains(r))
 				continue;
 
-			
-			float cross = (float) pq.cross(r.subtract(p));
+			float cross = pq.cross(r.subtract(p));
 			Circle c = makeCircumcircle(p, q, r);
 			if (c == null)
 				continue;
-			else if (cross > 0 && (left == null || pq.cross(c.getPoint().subtract(p)) > pq.cross(left.getPoint().subtract(p))))
+			else if (cross > 0
+					&& (left == null || pq.cross(c.getPoint().subtract(p)) > pq.cross(left.getPoint().subtract(p))))
 				left = c;
-			else if (cross < 0 && (right == null || pq.cross(c.getPoint().subtract(p)) < pq.cross(right.getPoint().subtract(p))))
+			else if (cross < 0
+					&& (right == null || pq.cross(c.getPoint().subtract(p)) < pq.cross(right.getPoint().subtract(p))))
 				right = c;
 		}
 
-		
 		if (left == null && right == null)
 			return circ;
 		else if (left == null)
@@ -74,11 +70,11 @@ public class SCE {
 
 	static Circle makeDiameter(Point a, Point b) {
 		Point c = new Point((a.x + b.x) / 2, (a.y + b.y) / 2);
-		return new Circle(c, (float) Math.max(c.distance(a), c.distance(b)));
+		return new Circle(c, Math.max(c.distance(a), c.distance(b)));
 	}
 
 	static Circle makeCircumcircle(Point a, Point b, Point c) {
-		
+
 		float ox = (Math.min(Math.min(a.x, b.x), c.x) + Math.max(Math.max(a.x, b.x), c.x)) / 2;
 		float oy = (Math.min(Math.min(a.y, b.y), c.y) + Math.max(Math.max(a.y, b.y), c.y)) / 2;
 		float ax = a.x - ox, ay = a.y - oy;
@@ -87,10 +83,10 @@ public class SCE {
 		float d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2;
 		if (d == 0)
 			return null;
-		float x = (float) (((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay)
-				+ (cx * cx + cy * cy) * (ay - by)) / d);
-		float y = (float) (((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx)
-				+ (cx * cx + cy * cy) * (bx - ax)) / d);
+		float x = (((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) + (cx * cx + cy * cy) * (ay - by))
+				/ d);
+		float y = (((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) + (cx * cx + cy * cy) * (bx - ax))
+				/ d);
 		Point p = new Point(ox + x, oy + y);
 		float r = Math.max(Math.max(p.distance(a), p.distance(b)), p.distance(c));
 		return new Circle(p, r);
