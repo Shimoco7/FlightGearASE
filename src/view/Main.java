6f0f91434172782c42ccb9d367b05f834bd28b2a
Shimon Cohen <Shimoco7@gmail.com>
2021-05-22 16:54:07 +0300

    Updated SceneBuilder view for Player.fxml Attached Player to MainWindow.fxml

--- src/view/Main.java
+++ src/view/Main.java
@@ -21,8 +21,9 @@ public class Main extends Application {
         MainWindowController myController = loader.getController();
         myController.setStage(primaryStage);
         primaryStage.setTitle("Flight-Gear Projector");
-        primaryStage.setScene(new Scene(root, 700, 617));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+        setWindowSize(primaryStage,800,600,600,600);
 
         MainWindowController mwc = loader.getController();
         mwc.setViewModel(vm);
@@ -34,5 +35,34 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void setWindowSize(Stage stage,int maxW,int minW, int maxH,int minH){
+        stage.widthProperty().addListener((o, oldValue, newValue)->{
+            if(newValue.intValue() < minW) {
+                stage.setResizable(false);
+                stage.setWidth(minW);
+                stage.setResizable(true);
+            }
+            if(newValue.intValue() > maxW) {
+                stage.setResizable(false);
+                stage.setWidth(maxW);
+                stage.setResizable(true);
+            }
+        });
+
+        stage.heightProperty().addListener((o,oldValue,newValue)->{
+            if(newValue.intValue()<minH){
+                stage.setResizable(false);
+                stage.setHeight(minH);
+                stage.setResizable(true);
+            }
+
+            if(newValue.intValue() > maxH) {
+                stage.setResizable(false);
+                stage.setHeight(maxH);
+                stage.setResizable(true);
+            }
+        });
     }
 }
