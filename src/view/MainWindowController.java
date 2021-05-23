ca9b1fdedffd5ca5515527f7d1d83f193f2b71d4
Shimon Cohen <Shimoco7@gmail.com>
2021-05-23 17:51:08 +0300

    Fixed Controllers issues

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,13 +1,11 @@
 package view;
 
-import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javafx.util.Duration;
 import view.player.Player;
 import viewmodel.ViewModel;
 
--- src/view/player/Player.java
+++ src/view/player/Player.java
@@ -7,14 +7,16 @@ import javafx.stage.Stage;
 import view.Main;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Player extends AnchorPane {
 
     public Player() {
         super();
-        FXMLLoader loader = new FXMLLoader();
         try {
-            AnchorPane player = loader.load((getClass().getResource("Player.fxml")));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Player.fxml"));
+            Parent root = (Parent)loader.load();
+            AnchorPane player = loader.load(getClass().getResource("Player.fxml"));
             PlayerController playerController = loader.getController();
             this.getChildren().add(player);
         } catch (IOException e) {
