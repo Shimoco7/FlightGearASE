a8b9d46fc6f9d024f3098845e3fd4a1110d86d8f
Shimon Cohen <Shimoco7@gmail.com>
2021-06-19 19:08:38 +0300

    finished logic painting for LinearRegPainter. need to fix line chart symbols colors and radius

--- src/ptm1/LinearRegPainter.java
+++ src/ptm1/LinearRegPainter.java
@@ -62,6 +62,7 @@ public class LinearRegPainter implements Painter{
             ApplicationStatus.setAppColor(Color.BLACK);
             ApplicationStatus.setAppFillColor("orange");
             ApplicationStatus.setAppStatusValue(selectedFeature + " does not have a correlated feature that fits the Linear-Regression Algorithm");
+            myChart.getData().clear();
             return;
         }
         else{
@@ -70,65 +71,91 @@ public class LinearRegPainter implements Painter{
         }
 
         if(!currFeature.equals(selectedFeature)){
-            myChart.getData().clear();
-            normalSeries.getData().clear();
-            anomalySeries.getData().clear();
-            lineSeries.getData().clear();
-            
-            ArrayList<Float> xValues = normalTs.getFeatureData(selectedFeature);
-            ArrayList<Float> yValues = normalTs.getFeatureData(correlatedFeature);
-            int len=xValues.size();
-            for(int i=0;i<len;i++){
-                normalSeries.getData().add(new XYChart.Data<>(xValues.get(i),yValues.get(i)));
-            }
-            myChart.getData().add(normalSeries);
-
-            ObservableList<Float> pointsX,pointsY;
-            if(timeStep>30){
-                pointsX = FXCollections.observableArrayList(anomalyTs.getFeatureData(selectedFeature).subList(timeStep-30,timeStep));
-                pointsY = FXCollections.observableArrayList(anomalyTs.getFeatureData(correlatedFeature).subList(timeStep-30,timeStep));
-
-            }
-            else{
-                pointsX = FXCollections.observableArrayList(anomalyTs.getFeatureData(selectedFeature).subList(0,timeStep));
-                pointsY = FXCollections.observableArrayList(anomalyTs.getFeatureData(correlatedFeature).subList(0,timeStep));
-            }
-            len=pointsX.size();
-            for(int i=0;i<len;i++){
-                anomalySeries.getData().add(new XYChart.Data<>(pointsX.get(i),pointsY.get(i)));
-            }
-            myChart.getData().add(anomalySeries);
-
-            Float max = Collections.max(normalTs.getFeatureData(selectedFeature));
-            Float min = Collections.min(normalTs.getFeatureData(selectedFeature));
-            lineSeries.getData().add(new XYChart.Data<>(min,line.f(min)));
-            lineSeries.getData().add(new XYChart.Data<>(max,line.f(max)));
-            myChart.getData().add(lineSeries);
-
-            Node node =myChart.lookup(".series0.chart-series-line");
-            node.setStyle("-fx-stroke: transparent;");
-
-            Node node1 =myChart.lookup(".series1.chart-series-line");
-            node1.setStyle("-fx-stroke: transparent;");
-
-            Node node2=myChart.lookup(".series2.chart-series-line");
-            node2.setStyle("-fx-stroke: grey;");
-
+            updateGraph(timeStep, selectedFeature, correlatedFeature, line);
             currFeature = selectedFeature;
         }
 
         else{
             if(timeStep<=oldTimeStep){
-
+                updateGraph(timeStep, selectedFeature, correlatedFeature, line);
             }
 
             else{
-
+                if(timeStep==oldTimeStep+1){
+                    Float x=anomalyTs.getFeatureData(selectedFeature).get(timeStep);
+                    Float y=anomalyTs.getFeatureData(correlatedFeature).get(timeStep);
+                    anomalySeries.getData().add(new XYChart.Data<>(x,y));
+                    if(timeStep>30){
+                        anomalySeries.getData().remove(0);
+                    }
+                    checkAnomaly(timeStep,selectedFeature,correlatedFeature);
+                }
+                else{
+                    updateGraph(timeStep, selectedFeature, correlatedFeature, line);
+                }
             }
         }
 
     }
 
+    private void updateGraph(int timeStep, String selectedFeature, String correlatedFeature, Line line) {
+        myChart.getData().clear();
+        normalSeries.getData().clear();
+        anomalySeries.getData().clear();
+        lineSeries.getData().clear();
+
+        ArrayList<Float> xValues = normalTs.getFeatureData(selectedFeature);
+        ArrayList<Float> yValues = normalTs.getFeatureData(correlatedFeature);
+        int len=xValues.size();
+        for(int i=0;i<len;i++){
+            normalSeries.getData().add(new XYChart.Data<>(xValues.get(i),yValues.get(i)));
+        }
+        myChart.getData().add(normalSeries);
+
+        ObservableList<Float> pointsX,pointsY;
+        if(timeStep>30){
+            pointsX = FXCollections.observableArrayList(anomalyTs.getFeatureData(selectedFeature).subList(timeStep-30,timeStep));
+            pointsY = FXCollections.observableArrayList(anomalyTs.getFeatureData(correlatedFeature).subList(timeStep-30,timeStep));
+
+        }
+        else{
+            pointsX = FXCollections.observableArrayList(anomalyTs.getFeatureData(selectedFeature).subList(0,timeStep));
+            pointsY = FXCollections.observableArrayList(anomalyTs.getFeatureData(correlatedFeature).subList(0,timeStep));
+        }
+        len=pointsX.size();
+        for(int i=0;i<len;i++){
+            anomalySeries.getData().add(new XYChart.Data<>(pointsX.get(i),pointsY.get(i)));
+        }
+        myChart.getData().add(anomalySeries);
+
+        Float max = Collections.max(normalTs.getFeatureData(selectedFeature));
+        Float min = Collections.min(normalTs.getFeatureData(selectedFeature));
+        lineSeries.getData().add(new XYChart.Data<>(min,line.f(min)));
+        lineSeries.getData().add(new XYChart.Data<>(max,line.f(max)));
+        myChart.getData().add(lineSeries);
+
+        Node node =myChart.lookup(".series0.chart-series-line");
+        node.setStyle("-fx-stroke: transparent;");
+
+        Node node1 =myChart.lookup(".series1.chart-series-line");
+        node1.setStyle("-fx-stroke: transparent;");
+
+        Node node2=myChart.lookup(".series2.chart-series-line");
+        node2.setStyle("-fx-stroke: grey;");
+
+        checkAnomaly(timeStep,selectedFeature,correlatedFeature);
+    }
+
+    private void checkAnomaly(int timeStep, String selectedFeature,String correlatedFeature) {
+        if(anomalyReports.containsKey(selectedFeature)){
+            if(anomalyReports.get(selectedFeature).contains(timeStep)){
+                ApplicationStatus.setAppColor(Color.BLACK);
+                ApplicationStatus.setAppFillColor("red");
+                ApplicationStatus.setAppStatusValue("Anomaly has been detected in ["+selectedFeature+"   ,   " + correlatedFeature+"] at " + Calculate.getTimeString(timeStep/10));
+                ApplicationStatus.pausePlayFromStart();
+            }
+        }
+    }
 
     @Override
     public void setAll(TimeSeries normalTs, TimeSeries anomalyTs, HashMap<String, HashSet<Integer>> anomalies) {
